ee1fad5c18c5153bbbd77d519abbd0bfea20e530
Marco André Mendes <marcoandre@gmail.com>
2022-06-20 17:12:43 -0300

    ajustes na visualização de status e itens de compra

--- core/serializers.py
+++ core/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework.serializers import ModelSerializer, CharField, SerializerMethodField
 
-from core.models import Autor, Categoria, Compra, Editora, Livro
+from core.models import Autor, Categoria, Compra, Editora, ItensCompra, Livro
 
 
 class CategoriaSerializer(ModelSerializer):
@@ -38,11 +38,6 @@ class LivroDetailSerializer(ModelSerializer):
     editora = EditoraNestedSerializer()
     autores = SerializerMethodField()
 
-    class Meta:
-        model = Livro
-        fields = "__all__"
-        depth = 1
-
     def get_autores(self, instance):
         # nomes_autores = []
         # autores = instance.autores.get_queryset()
@@ -51,9 +46,28 @@ class LivroDetailSerializer(ModelSerializer):
         # return nomes_autores
         return [autor.nome for autor in instance.autores.get_queryset()]
 
+    class Meta:
+        model = Livro
+        fields = "__all__"
+        depth = 1
+
+
+class ItensCompraSerializer(ModelSerializer):
+    total = SerializerMethodField()
+
+    def get_total(self, instance):
+        return instance.livro.preco * instance.quantidade
+
+    class Meta:
+        model = ItensCompra
+        fields = ("livro", "quantidade", "total")
+        depth = 2
+
 
 class CompraSerializer(ModelSerializer):
     usuario = CharField(source="usuario.email")
+    status = CharField(source="get_status_display")
+    itens = ItensCompraSerializer(many=True)
 
     class Meta:
         model = Compra
