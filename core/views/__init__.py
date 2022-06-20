a2434d17b2c08ad174b75cbfc33c6e0fb6cd1c2e
Marco André Mendes <marcoandre@gmail.com>
2022-06-20 14:53:46 -0300

    CRUD de autores

--- core/views/__init__.py
+++ core/views/__init__.py
@@ -1,6 +1,10 @@
 from .testes_iniciais import teste, teste2
+
 from .categoriaClass import CategoryView
-from .categoriaGeneric import CategoriaDetailGeneric, CategoriasListGeneric
+
 from .categoriaAPIView import CategoriaDetail, CategoriasList
+from .categoriaGeneric import CategoriaDetailGeneric, CategoriasListGeneric
+
+from .autor import AutorViewSet
 from .categoria import CategoriaViewSet
 from .editora import EditoraViewSet
--- livraria/urls.py
+++ livraria/urls.py
@@ -6,6 +6,7 @@ from rest_framework.routers import DefaultRouter
 from core import views
 
 router = DefaultRouter()
+router.register(r'autores', views.AutorViewSet)
 router.register(r'categorias', views.CategoriaViewSet)
 router.register(r'editoras', views.EditoraViewSet)
 
