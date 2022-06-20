8759598fc2976b5539c562a5f9103b033be34233
Marco André Mendes <marcoandre@gmail.com>
2022-06-20 15:48:41 -0300

    inclusão da ListDetailSerializer

--- core/views/livro.py
+++ core/views/livro.py
@@ -1,9 +1,16 @@
 from rest_framework.viewsets import ModelViewSet
 
-from core.models import Livro  # Importa o modelo do livro
-from core.serializers import LivroSerializer  # Importa o serializer do livro
+from core.models import Livro  
+from core.serializers import LivroSerializer, LivroDetailSerializer
 
 
 class LivroViewSet(ModelViewSet):
     queryset = Livro.objects.all()
-    serializer_class = LivroSerializer
+    # serializer_class = LivroSerializer
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return LivroDetailSerializer
+        if self.action == 'retrieve':
+            return LivroDetailSerializer
+        return LivroSerializer
+        
