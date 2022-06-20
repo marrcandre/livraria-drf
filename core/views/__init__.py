09acdde5f26bc7a4ef7be5f21f274aa5e3dcc505
Marco André Mendes <marcoandre@gmail.com>
2022-06-20 14:45:40 -0300

    CRUD Editora

--- core/views/__init__.py
+++ core/views/__init__.py
@@ -3,4 +3,4 @@ from .categoriaClass import CategoryView
 from .categoriaGeneric import CategoriaDetailGeneric, CategoriasListGeneric
 from .categoriaAPIView import CategoriaDetail, CategoriasList
 from .categoria import CategoriaViewSet
-
+from .editora import EditoraViewSet
--- livraria/urls.py
+++ livraria/urls.py
@@ -1,18 +1,3 @@
-"""livraria URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import include, path
 
@@ -21,14 +6,15 @@ from rest_framework.routers import DefaultRouter
 from core import views
 
 router = DefaultRouter()
-router.register(r'categorias-viewset', views.CategoriaViewSet)
+router.register(r'categorias', views.CategoriaViewSet)
+router.register(r'editoras', views.EditoraViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("teste/", views.teste),
     path("pag2/", views.teste2),
-    path("categorias/", views.CategoryView.as_view()),
-    path("categorias/<int:id>/", views.CategoryView.as_view()),
+    path("categorias-class/", views.CategoryView.as_view()),
+    path("categorias-class/<int:id>/", views.CategoryView.as_view()),
     path("categorias-apiview/", views.CategoriasList.as_view()),
     path("categorias-apiview/<int:id>/", views.CategoriaDetail.as_view()),
     path("categorias-generic/", views.CategoriasListGeneric.as_view()),
