98b8f997d7c5b9950a9190bc9b111fe0ed30e0db
Marco André Mendes <marcoandre@gmail.com>
2022-06-20 16:07:51 -0300

    representações via serializer

--- core/serializers.py
+++ core/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, CharField, SerializerMethodField
 
 from core.models import Autor, Categoria, Editora, Livro
 
@@ -15,6 +15,12 @@ class EditoraSerializer(ModelSerializer):
         fields = "__all__"
 
 
+class EditoraNestedSerializer(ModelSerializer):
+    class Meta:
+        model = Editora
+        fields = ("nome",)
+
+
 class AutorSerializer(ModelSerializer):
     class Meta:
         model = Autor
@@ -28,7 +34,19 @@ class LivroSerializer(ModelSerializer):
 
 
 class LivroDetailSerializer(ModelSerializer):
+    categoria = CharField(source="categoria.descricao")
+    editora = EditoraNestedSerializer()
+    autores = SerializerMethodField()
+
     class Meta:
         model = Livro
         fields = "__all__"
         depth = 1
+
+    def get_autores(self, instance):
+        # nomes_autores = []
+        # autores = instance.autores.get_queryset()
+        # for autor in autores:
+        #     nomes_autores.append(autor.nome)
+        # return nomes_autores
+        return [autor.nome for autor in instance.autores.get_queryset()]
